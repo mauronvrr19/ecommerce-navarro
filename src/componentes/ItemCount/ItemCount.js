5a4c435762c5ff90cdccc448d3a68481dbd7e562
mauronvrr19 <mauronvrr19@gmail.com>
2021-12-13 21:18:45 -0300

    react

--- src/componentes/ItemCount/ItemCount.js
+++ src/componentes/ItemCount/ItemCount.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import ItemListContainer from "../ItemListContainer";
 
-const ItemCount = ({ stock, initial})=> {
+const ItemCount = ({ stock, initial,onAdd})=> {
 let [contador, setContador] = useState(0);
 
 const quitar =()=> {
@@ -22,21 +23,17 @@ console.log("stock maximo")
     setContador(contador + 1)
  }
     }
-
-    const onAdd =()=> {
-        if (contador > 0 & contador <= 10){
-        console.log("compra efectuada")}
-        else {
-            console.log("debe seleccionar un producto")
-        }
-    }
+const confirmar =()=>{
+    onAdd()
+}
 
     return (
 
 <div className="btn-group" role="group" aria-label="Basic example">
 <p>Contador Actual : {contador}</p>
   <button onClick={quitar} type="button" className="btn btn-primary">quitar</button>
-  <button onClick={onAdd} type="button" className="btn btn-primary">confirmar</button>
+  <button onClick={confirmar} type="button" className="btn btn-primary">confirmar</button>
+
   <button onClick={agregar} type="button" className="btn btn-primary">agregar</button>
 </div>
     )
--- src/componentes/itemListContainer.js
+++ src/componentes/itemListContainer.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ItemList from "./ItemCount/ItemList";
 import Item from "./ItemCount/Item"
+import ItemCount from "./ItemCount/ItemCount";
 
 const ItemListContainer = (props) =>
 {
+    const onAdd = () => console.log("producto aniadido")
+
     return(
         <div>
         <ul>
@@ -11,9 +14,10 @@ const ItemListContainer = (props) =>
             <li> {props.nombre} 2 </li>
             <li> {props.nombre} 3</li>
             <li> {props.nombre} 4</li>
-        </ul>
-        
-    {id.map(ids => <ItemList/>)}
+        </ul>      
+        <ItemCount stock={10} initial={1} onAdd={onAdd}/>
+  
+    {/* {id.map(ids => <ItemList/>)} */}
 </div>
     )
 
