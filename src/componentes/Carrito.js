4ba6761fb197a0dc307d34ffa26d11f03b316abc
mauronvrr19 <mauronvrr19@gmail.com>
2022-01-11 23:18:57 -0300

    react

--- src/componentes/Carrito.js
+++ src/componentes/Carrito.js
@@ -35,18 +35,19 @@ const Carrito = () => {
 //     )
 // }
 // export default Carrito
-
 return (
     <div>
+        Soy Carrito
         {carrito.length > 0 ? (
             <ul>
-              {carrito.map((unidad, index) => {
-                        return <li key={index}>{unidad.title}  ${unidad.price}</li>
-                    }) }
-                          <div>{console.log(carrito + "carrito")}</div>  
-          <button onClick={carrito} type="button" className="btn btn-primary">carrito</button>
-  <button onClick={borrarDelCarrito} type="button" className="btn btn-primary">borrar</button>
- <button onClick={limpiarCarrito} type="button" className="btn btn-primary">limpiar</button>
+                {carrito.map((unidad, indice) => {
+                    return (
+                        <li key={indice}>
+                            {unidad.title} - ${unidad.price}
+                            <button onClick={()=>borrarDelCarrito(unidad.id,unidad.contador)}>borrar</button>
+                        </li>
+                    )
+                })}
             </ul>
         ) : <p>No hay productos en el carrito</p>}
 
